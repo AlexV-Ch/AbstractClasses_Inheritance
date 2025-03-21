c537d7bde348b35b7d1fc0d21b89439b6523dce7
AlexV-Ch <alexch@yandex.ru>
2025-03-21 20:52:58 +0300

    14.9 Практическая работа начало 3

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         Company company = new Company();
         company.hire(new Operator(100));
+        company.hire(new Operator(120));
         company.hire(new Manager(300));
+        company.hire(new Manager(350));
         company.hire(new TopManager(500));
+        company.hire(new TopManager(550));
     }
 }
